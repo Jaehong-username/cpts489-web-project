b5461a447640d9d2df2d9cdd0499a67b0ba1c05d
Jaehong-username <ehoas7137@gmail.com>
2025-03-01 07:10:11 -0800

    Created About Page

---
+++ login.js
@@ -0,0 +1,22 @@
+function validateEmail() {//document part of web api
+    const email = document.getElementById("email").value; //instead og grabbing the element node we get an attribute
+    var email_valid = false;
+    const valid_first = email.includes("@"); //return boolean
+    
+    if (valid_first) email_valid = true;
+    
+    if (!email_valid) {
+        alert("Email is bad. Fix it!\n");
+    }
+    
+    
+    const password = document.getElementById("password").value;
+    var password_valid = false;
+    const valid_second = password.Length > 10; //return boolean
+    if (valid_second) password_valid = true;
+    if (!password_valid) {
+        alert("Password should be longer than 10 characters. Fix it!\n");
+    }
+    
+    return (email_valid && password_valid); 
+}
