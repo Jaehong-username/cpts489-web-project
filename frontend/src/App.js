2ea16657a9d9a704bbcb5880ed5b898581b47865
JonahChesnut <jonah.chesnut@wsu.edu>
2025-04-08 15:48:36 -0700

    Added load board placeholder file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import HomePage from './pages/HomePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import TruckersPage from './pages/TruckersPage.jsx';
 import BrokersPage from './pages/BrokersPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import LoadBoard from './pages/LoadBoard.jsx';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
@@ -20,6 +22,8 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/truckers" element={<TruckersPage />} />
             <Route path="/brokers" element={<BrokersPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/loadboard" element={<LoadBoard />} />
           </Routes>
         </main>
         <Footer />
