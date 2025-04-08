f9c89cd409ee2c81626a67dcad5e6864abf265b4
JonahChesnut <jonah.chesnut@wsu.edu>
2025-04-08 13:27:22 -0700

    Moved images to public and converted BrokersPage to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import TruckersPage from './pages/TruckersPage.jsx';
+import BrokersPage from './pages/BrokersPage.jsx';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/truckers" element={<TruckersPage />} />
+            <Route path="/brokers" element={<BrokersPage />} />
           </Routes>
         </main>
         <Footer />
